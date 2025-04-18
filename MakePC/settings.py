128e42b31bfdd4b44a413c049acc8e2cb30785d6
ernazarov51 <ravshantursunboyev55@gmail.com>
2025-04-18 18:37:36 +0500

    add requirements

--- MakePC/settings.py
+++ MakePC/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR , "static")
