495ea3c8ff4f5e6bbbdbbc746a5801479610e16f
ernazarov51 <ravshantursunboyev55@gmail.com>
2025-04-22 14:59:22 +0500

    k

--- apps/urls.py
+++ apps/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.views import RegisterCreateAPIView
+from apps.views import RegisterCreateAPIView, user_profile_api_view
 
 urlpatterns=[
     path('register/',RegisterCreateAPIView.as_view(),name='register'),
-    path('profile/',RegisterCreateAPIView.as_view(),name='register')
+    path('profile/',user_profile_api_view,name='register')
 ]
