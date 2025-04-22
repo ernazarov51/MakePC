886e3c13880ae6a8a3edb6869f08020f020c96a1
ernazarov51 <ravshantursunboyev55@gmail.com>
2025-04-22 15:06:07 +0500

    k

--- apps/views.py
+++ apps/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.generics import CreateAPIView,RetrieveAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -14,19 +14,17 @@ from apps.serializers import RegisterModelSerializer, CustomTokenObtainPairSeria
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
+
 # Create your views here.
 class RegisterCreateAPIView(CreateAPIView):
     serializer_class = RegisterModelSerializer
     queryset = User.objects.all()
 
+
 @extend_schema(tags=['Customer'])
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_profile_api_view(request):
-    user=request.user
-    serializer=ProfileModelSerializer(request.user)
-    if serializer.is_valid():
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-
+    user = request.user
+    serializer = ProfileModelSerializer(instance=user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
